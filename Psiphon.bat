9b908337c67d39661567bd2ce5011962a45d877b
おとめ座 ひなたぼっこ <sonvirgo@users.noreply.github.com>
2023-10-26 13:38:46 +0700

    Add files via upload
    migrate from sstap-beta to tun2socks on windows

--- Psiphon.bat
+++ Psiphon.bat
@@ -34,26 +34,62 @@ if errorlevel 1 (
 
 )  
 
-cd C:\Users\virgosun\AppData\Local\SSTap-beta
+rem cd C:\Users\virgosun\AppData\Local\SSTap-beta
 
-start "" C:\Users\virgosun\AppData\Local\SSTap-beta\SSTap.exe
+rem start "" C:\Users\virgosun\AppData\Local\SSTap-beta\SSTap.exe
 
-:FOUND2
+rem  :FOUND2
 
-netsh interface show interface|find "Connected"|find "SSTAP 1"
+rem  netsh interface show interface|find "Connected"|find "SSTAP 1"
+
+rem  if errorlevel 1 (
+    
+rem    echo SSTap not found
+
+rem    TIMEOUT /T 1	
+
+rem    goto FOUND2
+
+rem  )
+
+start ""   tun2socks-windows-amd64 -device tun://wintun?guid={7FE5A4A1-1326-40E2-974C-EF617156ACA8} -proxy socks5://127.0.0.1:1081
+
+:FOUND3
+
+netsh interface show interface|find "Connected"|find "wintun"
 
 if errorlevel 1 (
     
-   echo SSTap not found
+  echo wintun not found
 
-   TIMEOUT /T 1	
+  TIMEOUT /T 1	
 
-   goto FOUND2
+  goto FOUND3
 
-)  
+)
 
-C:\Portables\htpdate\htpdate.exe  -s -C "[time ]HH:mm:ss.SS[ && date ]MM-DD-YY" -p http google.com
+netsh interface ip set address name="wintun" source=static addr=192.168.123.1 mask=255.255.255.0 gateway=none
+
+rem netsh interface ipv4 set interface wintun metric=6 
+
+rem netsh int ipv4 add route 0.0.0.0/0 wintun 192.168.123.1   
+
+for /f "tokens=*" %%i in ('netsh int ipv4 show interfaces ^| find "wintun"') do set str=%%i
+
+set /A num=str
+
+route delete 0.0.0.0 mask 0.0.0.0 192.168.123.1 
+
+route add 0.0.0.0 mask 0.0.0.0 192.168.123.1 IF %num% metric 5
+
+C:\tools\htpdate\htpdate.exe  -s -C "[time ]HH:mm:ss.SS[ && date ]MM-DD-YY" -p http google.com
+
+powershell -Command "Disable-Ics"
+
+TIMEOUT /T 1	
+
+powershell -Command "Set-Ics wintun Ethernet"
 
 rem C:\Tools\PersistentHotspot\PersistentHotspot.exe
 
-netsh wlan start hostednetwork
+rem netsh wlan start hostednetwork
