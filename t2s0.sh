497fd64d98d42bc5e95d68983000cbe2205c09a5
おとめ座 ひなたぼっこ <sonvirgo@users.noreply.github.com>
2023-12-06 22:34:35 +0700

    Update t2s0.sh

--- t2s0.sh
+++ t2s0.sh
@@ -1,10 +1,17 @@
 
 #!/usr/bin/env bash
-#/Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | awk '/ SSID:/ {print $2}' | grep 'Code.Gurus' > /dev/null && {
-#cd  /Applications/t2s 
+cd ~
+while ! /Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | awk '/ SSID:/ {print $2}' | grep 'Code.gurus' > /dev/null  ;
+do
+  sleep 1
+  #printf 'wifi not connected ... try connecintg ...'
+  networksetup -setairportnetwork en1 Code.gurus 12345678
+done
+
+{
 pass=1234
-nohup echo pass | sudo -S  ./tun2socks-darwin-amd64 -device utun123 -proxy socks5://192.168.43.134:2088 -interface en0  > /dev/null 2>&1&
-sleep 10
+nohup echo $pass | sudo -S  ./tun2socks-darwin-amd64  -device utun123 -proxy socks5://192.168.43.134:2088 -interface en1  > /dev/null 2>&1&
+sleep 5
 echo $pass | sudo -S ifconfig utun123 198.18.0.1 198.18.0.1 up
 echo $pass | sudo -S route add -net 1.0.0.0/8 198.18.0.1
 echo $pass | sudo -S route add -net 2.0.0.0/7 198.18.0.1
@@ -15,7 +22,7 @@ echo $pass | sudo -S route add -net 32.0.0.0/3 198.18.0.1
 echo $pass | sudo -S route add -net 64.0.0.0/2 198.18.0.1
 echo $pass | sudo -S route add -net 128.0.0.0/1 198.18.0.1
 echo $pass | sudo -S route add -net 198.18.0.0/15 198.18.0.1
-#cd /Applications/httpdate-main
-echo $pass  | sudo -S ./htpdate-mac-x64  -s -C  '[date  ]MMDDHHmmYY'  http://142.250.105.139 
-#}
+echo $pass  | sudo -S ./htpdate-mac-x64  -s -C  '[date  ]MMDDHHmmYY'  http://142.250.105.139
+}
+
 
