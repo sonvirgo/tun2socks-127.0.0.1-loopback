910abc891b468a6c4ca42e8e9541ee1efd7ba0cb
おとめ座 ひなたぼっこ <sonvirgo@users.noreply.github.com>
2024-07-20 10:31:47 +0700

    Add files via upload

--- ut2s0.sh
+++ ut2s0.sh
@@ -14,6 +14,12 @@ echo $pass | sudo -S route delete  -net 125.235.36.0/24 192.168.1.250
 #end exp
 
 
+echo $pass | sudo -S killall  psiphon-tunnel-core-x86_64
+echo $pass | sudo -S killall  tun2socks-darwin-amd64
+echo $pass | sudo -S killall  go-dispatch-proxy
+echo $pass | sudo -S killall  MTOSocks5-x86_64
+
+
 # nohup echo $pass | sudo -S  ./tun2socks-darwin-amd64  -device utun123 -proxy socks5://127.0.0.1:2088 -interface en0  > /dev/null 2>&1&
 sleep 5
 # echo $pass | sudo -S ifconfig utun123 198.18.0.1 198.18.0.1 up
@@ -28,5 +34,4 @@ echo $pass | sudo -S route delete -net 128.0.0.0/1 198.18.0.1
 echo $pass | sudo -S route delete  -net 198.18.0.0/15 198.18.0.1
 # echo $pass  | sudo -S ./htpdate-mac-x64  -s -C  '[date  ]MMDDHHmmYY'  http://142.250.105.139
 }
-
 
