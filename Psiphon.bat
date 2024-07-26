db9020f0f82218b476027a329fd01eb6bd3ce84a
おとめ座 ひなたぼっこ <sonvirgo@users.noreply.github.com>
2024-07-26 09:13:35 +0700

    Add files via upload

--- Psiphon.bat
+++ Psiphon.bat
@@ -2,31 +2,51 @@
 d:
 cd  d:\progs\VPN
 
-rem :FOUND0
+:FOUND0
 
-rem netsh interface show interface|find "Connected"|find "Ethernet 2"
+netsh interface show interface|find "Connected"|find "Ethernet"
 
-rem if errorlevel 1 (
+if errorlevel 1 (
     
-rem    echo Wifi not found
+   echo Wifi/Ethenet not connected
 
-rem   TIMEOUT /T 1	
+   TIMEOUT /T 1	
 
-rem   goto FOUND0
+   goto FOUND0
 
-rem )  
+)  
 
-route add 125.235.0.0 MASK 255.255.0.0 192.168.1.250
+rem netsh int ipv4 add route 125.235.36.0/24 "Ethernet" 192.168.1.250
+
+for /f "tokens=*" %%i in ('netsh int ipv4 show interfaces ^| find "Ethernet"') do set str=%%i
+
+set /A num2=str
+
+route add 125.235.0.0 MASK 255.255.0.0 192.168.1.250 IF %num2% metric 4
+
+rem route add 8.219.113.0 mask 255.255.255.0 192.168.1.250 IF %num2% metric 4
+
+rem route add 171.244.232.0 mask 255.255.255.0 192.168.1.250 IF %num2% metric 4
+
+rem route add 203.190.171.0 mask 255.255.255.0 192.168.1.250 IF %num2% metric 4
+
+rem route add 172.66.43.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
+
+rem route add 104.26.14.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
+
+rem route add 104.26.15.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
+
+rem route add 172.67.74.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
 
 for /f "tokens=3 delims=: " %%i in ('netsh interface ip show config name^="Ethernet" ^| findstr "IP Address"') do set IP=%%i
 
-d:\progs\VPN\ForceBindIP64.exe -i %IP% d:\progs\VPN\dispatch.exe start  %IP% 
+rem d:\progs\VPN\ForceBindIP64.exe -i %IP% 
+
+start /B d:\progs\VPN\go-dispatch-proxy.exe -lport 1080  %IP% 
+
 start /B  d:\progs\VPN\MTOS5.exe 
 
-
 start /B d:\progs\VPN\psiphon-tunnel-core-x86_64.exe -config d:\progs\VPN\psiphone.conf -serverList d:\progs\VPN\psiphon-embedded-server-entries.txt
-rem -scan 103.84.77.0/24
-
 
 :FOUND1
 
@@ -42,75 +62,53 @@ if errorlevel 1 (
 
 )  
 
-TIMEOUT /T 10	
+start /B  tun2socks-windows-amd64 -device tun://wintun?guid={7FE5A4A1-1326-40E2-974C-EF617156ACA8} -proxy socks5://127.0.0.1:3080 -interface "Ethernet"
 
-cd C:\Users\virgosun\AppData\Local\SSTap-beta
+:FOUND3
 
-start "" C:\Users\virgosun\AppData\Local\SSTap-beta\SSTap.exe
-
-:FOUND2
-
-netsh interface show interface|find "Connected"|find "SSTAP 1"
+netsh interface show interface|find "Connected"|find "wintun"
 
 if errorlevel 1 (
     
-echo SSTap not found
+   echo wintun not found
 
-TIMEOUT /T 1	
+   TIMEOUT /T 1	
 
-goto FOUND2
+   goto FOUND3
 
 )
 
-rem start /B  tun2socks-windows-amd64 -device tun://wintun?guid={7FE5A4A1-1326-40E2-974C-EF617156ACA8} -proxy socks5://127.0.0.1:1081
+netsh interface ip set address name="wintun" source=static addr=192.168.123.1 mask=255.255.255.0 gateway=none
 
-rem :FOUND3
+netsh interface ipv4 set interface wintun metric=6 
 
-rem netsh interface show interface|find "Connected"|find "wintun"
+rem start /B  d:\progs\VPN\dispatch.exe start --port 3080  %IP%@7 192.168.123.1@3
+
+rem cd C:\Users\virgosun\AppData\Local\SSTap-beta
+
+rem start "" C:\Users\virgosun\AppData\Local\SSTap-beta\SSTap.exe
+
+rem :FOUND2
+
+rem netsh interface show interface|find "Connected"|find "SSTAP 1"
 
 rem if errorlevel 1 (
     
-rem   echo wintun not found
+rem echo SSTap not found
 
-rem   TIMEOUT /T 1	
+rem TIMEOUT /T 1	
 
-rem   goto FOUND3
+rem goto FOUND2
 
 rem )
 
-rem netsh interface ip set address name="wintun" source=static addr=192.168.123.1 mask=255.255.255.0 gateway=none
-
-rem netsh interface ipv4 set interface wintun metric=6 
-
 rem netsh int ipv4 add route 0.0.0.0/0 wintun 192.168.123.1   
 
-rem for /f "tokens=*" %%i in ('netsh int ipv4 show interfaces ^| find "wintun"') do set str=%%i
+for /f "tokens=*" %%i in ('netsh int ipv4 show interfaces ^| find "wintun"') do set str=%%i
 
-rem set /A num=str
+set /A num=str
 
-rem for /f "tokens=*" %%i in ('netsh int ipv4 show interfaces ^| find "Ethernet 2"') do set str=%%i
-
-rem set /A num2=str
-
-rem route delete 0.0.0.0 mask 0.0.0.0 192.168.123.1 
-
-rem route add 0.0.0.0 mask 0.0.0.0 192.168.123.1 IF %num% metric 5
-
-rem route add 125.235.36.0 mask 255.255.255.0 192.168.1.250 IF %num2% metric 4
-
-rem route add 8.219.113.0 mask 255.255.255.0 192.168.1.250 IF %num2% metric 4
-
-rem route add 171.244.232.0 mask 255.255.255.0 192.168.1.250 IF %num2% metric 4
-
-rem route add 203.190.171.0 mask 255.255.255.0 192.168.1.250 IF %num2% metric 4
-
-rem route add 172.66.43.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
-
-rem route add 104.26.14.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
-
-rem route add 104.26.15.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
-
-rem route add 172.67.74.0 mask 255.255.255.0 192.168.1.1 IF %num2% metric 4
+route add 0.0.0.0 mask 0.0.0.0 192.168.123.1 IF %num% metric 5
 
 cd  d:\progs\VPN
 
